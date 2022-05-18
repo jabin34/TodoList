65b369debbf3c20efbbce91c2376a2ce2cfadf5d
jabin34 <isratjabinesha@gmail.com>
2022-05-19 00:33:25 +0900

    add task form done (ui)

--- todo/src/Home/Home.js
+++ todo/src/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoForm from './TodoForm';
 
 const Home = () => {
     return (
-        <div>
-            home
+        <div className='flex  sm:flex-column md:flex-row justify-around g-5'>
+           <TodoForm/>
+           
         </div>
     );
 };
---
+++ todo/src/Home/TodoForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const TodoForm = () => {
+    const addTask =(e)=>{
+        e.preventDefault();
+     const taskName = e.target.name.value;
+     const description = e.target.description.value;
+     console.log(taskName,description);
+    }
+    return (
+        <div>
+            <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Add Your Task here</h2>
+    <div>
+        <form onSubmit={addTask}>
+    <div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Task Name</span>
+  </label>
+  <input name='name' type="text" placeholder="your task name..." class="input input-bordered w-full max-w-xs" required />
+</div>
+<label class="label">
+    <span class="label-text mt-4">Description</span>
+  </label>
+<textarea  name='description' type="text" class="textarea textarea-bordered w-full max-w-xs " placeholder="description...." required></textarea>
+   <div class="card-actions justify-center mt-4">
+      <input class="btn btn-md "  type="submit" value="Add Task"/>
+    </div>
+        </form>
+    </div>
+    
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default TodoForm;
